2b375b6792b4e5d5984f2c76e2efa70aac6bc34b
Francesco Velo <velofrancesco9@gmail.com>
2025-12-03 16:40:56 +0100

    Added new customer exists filter

---
+++ Ciclilavarizia/Filters/EnsureCustomerExists.cs
@@ -0,0 +1,33 @@
+﻿using Ciclilavarizia.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ciclilavarizia.Filters
+{
+    public class EnsureCustomerExists : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            int? id = (int?)context.ActionArguments["customerId"] ?? null;
+            if (id == null || id < 0)
+            {
+                context.Result = new BadRequestObjectResult($"Invalid ID provided for customerId.");
+                return;
+            }
+
+            var db = context.HttpContext.RequestServices.GetRequiredService<CiclilavariziaDevContext>();
+            bool exists = await db.Customers
+                .AsNoTracking()
+                .AnyAsync(c => c.CustomerID == id, context.HttpContext.RequestAborted);
+
+            if (!exists)
+            {
+                context.Result = new NotFoundResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
