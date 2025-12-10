1b0b98d8fcbaef46145031e55228d1007a2971a1
Francesco Velo <velofrancesco9@gmail.com>
2025-12-10 10:23:38 +0100

    created filters for testing

---
+++ Ciclilavarizia/Filters/PrintModelStateVariables.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Ciclilavarizia.Filters
+{
+    public class PrintModelStateVariables: Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Console.WriteLine("--------------------PrintModelStateVariables--------------------");
+
+            foreach(KeyValuePair<string, object> kvp in context.ActionArguments)
+            {
+                Console.WriteLine($"\tKey: {kvp.Key} - Value: {kvp.Value}.s");
+            }
+
+            Console.WriteLine("--------------------PrintModelStateVariables--------------------");
+            next();
+        }
+    }
+}
