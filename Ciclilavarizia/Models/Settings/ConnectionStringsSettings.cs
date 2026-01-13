314568530e3f74cdfdbf767b84f3417defe93b0b
Francesco Velo <velofrancesco9@gmail.com>
2026-01-13 16:10:23 +0100

    deleted unused files

--- Ciclilavarizia/Models/Settings/ConnectionStringsSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ciclilavarizia.Models.Settings
-{
-    public class ConnectionStringsSettings
-    {
-        public string AdventureWorksLTDbHomelab { get; set; } = string.Empty;
-    }
-}
