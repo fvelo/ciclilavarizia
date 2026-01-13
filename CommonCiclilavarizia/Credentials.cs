6268e3904105fc86b65d18cd6c18a3e188867e18
Francesco Velo <velofrancesco9@gmail.com>
2026-01-13 16:16:37 +0100

    More complete model for Crendentials

--- CommonCiclilavarizia/Credentials.cs
+++ CommonCiclilavarizia/Credentials.cs
@@ -10,12 +10,16 @@ namespace CommonCiclilavarizia
         [Required]
         public long CustomerId { get; set; }
         [Required]
+        [StringLength(50)]
         public string EmailAddress { get; set; }
         [Required]
+        [StringLength(128)]
         public string PasswordHash { get; set; }
         [Required]
+        [StringLength(10)]
         public string PasswordSalt { get; set; }
         [Required]
+        [StringLength(20)]
         public string Role { get; set; }
 
         public Credentials() { }
